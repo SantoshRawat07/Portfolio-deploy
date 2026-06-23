f660b23e845c0d35d09066ede31026a17466ed7e
SantoshRawat07 <santoshchettri216@gmail.com>
2026-06-23 22:46:44 +0545

    commit last

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRouter from './Home.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import Loader from './assets/Components/Loader/Loader.jsx'
 
