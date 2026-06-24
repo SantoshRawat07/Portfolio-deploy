c27226b940527605b11dfd3a6da6f52ce50d4f2f
SantoshRawat07 <santoshchettri216@gmail.com>
2026-06-24 23:14:38 +0545

    last update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 import Loader from './assets/Components/Loader/Loader.jsx'
-import App from './App.jsx'
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
