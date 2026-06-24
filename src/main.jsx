6a689517493bb3aa86f37d3b685ad0c3d49bdde5
SantoshRawat07 <santoshchettri216@gmail.com>
2026-06-24 11:10:38 +0545

    last update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import Loader from './assets/Components/Loader/Loader.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
+import Loader from './assets/Components/Loader/Loader.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
   <StrictMode>
-    <Loader/>
-    <AppRouter/>
+    <BrowserRouter>
+      <Loader />
+      <App />
+    </BrowserRouter>
   </StrictMode>
-  </BrowserRouter>  
 )
